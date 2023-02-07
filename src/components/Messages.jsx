39a525d0816eb346beeaefa5da1f483263d67d6b
Gerardo Castañeda <zl1fmercury@gmail.com>
2023-02-06 23:32:12 -0600

    add message components to view

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import Message from "./Message";
 
 const Messages = () => {
-  <div className="messages">
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-  </div>;
+  return (
+    <div className="messages">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
 };
 
 export default Messages;
