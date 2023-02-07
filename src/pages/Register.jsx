afc63e042aff256ffa266aed0fe7e33a58e09b66
Gerardo Castañeda <zl1fmercury@gmail.com>
2023-02-07 14:51:11 -0600

    add database to store users

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import addAvatar from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      // Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        (error) => {
+          setError(true);
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          });
+        }
+      );
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <div className="form-container">
       <div className="form-wrapper">
         <span className="logo">Chat App</span>
         <span className="title">Register</span>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input type="text" name="name" id="name" placeholder="Name" />
-          <input type="email" name="email" id="name" placeholder="Email" />
+          <input type="email" name="email" id="email" placeholder="Email" />
           <input
             type="password"
             name="password"
@@ -23,14 +78,20 @@ const Register = () => {
             style={{ display: "none" }}
           />
           <label htmlFor="file">
-            <img src={addAvatar} alt="add avatar file input" width={48} height={48} />
+            <img
+              src={addAvatar}
+              alt="add avatar file input"
+              width={48}
+              height={48}
+            />
             <span>Add an avatar</span>
           </label>
           <button type="submit">Sign Up</button>
-          <p>
-            Aready have an account? <span>Login </span>
-          </p>
+          {error && <span>Something went wrong</span>}
         </form>
+        <p>
+          Aready have an account? <span>Login </span>
+        </p>
       </div>
     </div>
   );
